f25a87bf71a89e35cd23d8b9bcf05c1107c3ba76
Evelyn Coronel <astrocronopio@hotmail.com>
2020-04-02 18:54:05 +0100

    update

---
+++ pr-rn-4/Codigo/pr-rn-4.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+#include <math.h>
+#include <string>
+#include <sstream>
+
+
+void create_weights(int N)
+{https://mail.google.com/mail/u/0/#search/mato/FMfcgxwHMZQrGHzPkNSPCZwTqHCdLbbh
+
+}
+
+void initialize (double * matrix)
+{
+
+}
+
+void destroy_matrix(double * matrix )
+{
+
+
+}
+
+
+void xor_nn() //entrada 1  entrada 2
+{
+
+}
