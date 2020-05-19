1b2872ef119b6c2c0aa1bcf9a8e5480674eb9218
Evelyn Coronel <astrocronopio@hotmail.com>
2020-05-19 15:37:00 -0300

    terminé el informe

--- pr-rn-6/Codigo/plot-pr-rn-6.py
+++ pr-rn-6/Codigo/plot-pr-rn-6.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import matplotlib as mpl
-mpl.rcParams.update({'font.size': 19,  'figure.figsize': [10, 8],  'font.family': 'serif', 'font.sans-serif': ['Helvetica']})
+mpl.rcParams.update({'font.size': 19,  'figure.figsize': [10, 7],  'font.family': 'serif', 'font.sans-serif': ['Helvetica']})
 
 colormap = plt.cm.gist_ncar
 
 
-def ejer_1():
-	N_vec=[500]#[500, 1000 , 2000, 4000]
+def ejer_1(n):
+	N_vec=[n]#[500, 1000 , 2000, 4000]
 	alpha_vec = [0.12, 0.14, 0.16,0.18]
 
 	files = []
@@ -55,6 +55,56 @@ def ejer_1():
 	plt.savefig("../Graficos/{}.png".format(N_vec[0]))
 
 
+
+def ejer_1_2():
+	N_vec=[1000]#[500, 1000 , 2000, 4000]
+	alpha_vec = [0.09, 0.1, 0.12,0.15]#[0.12, 0.14, 0.16, 0.18]
+
+
+	files = []
+
+	for N in N_vec: 
+		for alpha in alpha_vec: 
+			files.append("./Data_files/test_alpha{}_{}".format(N, alpha))
+
+	f, ax = plt.subplots(2, 2, sharey=True, sharex=True)
+	f.subplots_adjust(hspace=0, wspace=0, top=0.95, right=0.95)
+	#f.suptitle("Para $N={}$".format(N_vec[0]))
+	
+	counterx=0
+	countery=0
+	counter=0
+	
+
+	ax[counterx][countery].set_ylabel("$N_{cuentas}/N_{patrones}$")
+
+	for filename  in files:
+		binwidth=0.05
+		m = np.loadtxt(filename, unpack=True)
+		bins=np.arange(min(m), max(m) + binwidth, binwidth)
+		weights = np.ones_like(m) / len(m)
+		ax[counterx][countery].set_xticks(np.arange(0, 1.2, step=0.2)) 
+
+		#ax[counterx][countery].set_title("$\\alpha$="+str(alpha_vec[counter]))
+		
+		ax[counterx][countery].hist(m, weights=weights, alpha=0.5, color='blue', bins=bins, label="$\\alpha$="+str(alpha_vec[counter]))
+		ax[counterx][countery].set_xlim(0.1,1.1)
+		ax[counterx][countery].set_ylim(0.01,1.1)
+		ax[counterx][countery].legend(loc='upper left', handlelength=0)
+		
+		counter+=1
+		countery+=1
+		countery=countery%2
+		if counter==2:
+				counterx=1
+				ax[counterx][countery].set_ylabel("$N_{cuentas}/N_{patrones}$")
+		if counterx==1:
+				ax[counterx][countery].set_xlabel(" $m_\\mu$")
+
+		
+	plt.savefig("../Graficos/{}_alpha.png".format(N_vec[0]))
+
+
 def ejer_2():
 	N=4000
 	p=40
@@ -62,12 +112,12 @@ def ejer_2():
 	files=np.array([])
 	mean_files=np.array([])
 	stdev_files=np.array([])
-	t = np.arange(0.1, 2.1, 0.1)
+	t = np.arange(0.1, 2.1 ,0.1 )
 	beta_vec=[]
 
 	for T in t:
-		files=np.append(files, "./Data_files/temp{}".format(T))
-		m = np.loadtxt("./Data_files/temp{}".format(T), unpack=True)
+		files=np.append(files, "./Data_files/temp_alter{}".format(T))
+		m = np.loadtxt("./Data_files/temp_alter{}".format(T), unpack=True)
 		
 		mean = np.mean(m)
 		stdev= np.std(m)
@@ -80,17 +130,77 @@ def ejer_2():
 	print(t)
 	print(files)
 	print(mean_files)
-	plt.plot(beta_vec, mean_files, color='red', alpha=0.8)
 
+	N1=1000
+	p1=100
+	t1 = np.arange(2.0, 0.0, -0.1)
+	print(t1)
+
+	files1=np.array([])
+	mean_files1=np.array([])
+	stdev_files1=np.array([])
+	beta_vec1=[]
+
+	for T in t1:
+		files1=np.append(files1, "./Data_files/temp_alpha_0_10{}".format(T))
+		m = np.loadtxt("./Data_files/temp_alpha_0_10{}".format(T), unpack=True)
+		
+		mean = np.mean(m)
+		stdev= np.std(m)
+
+		beta = T
+		mean_files1=np.append(mean_files1,mean)
+		stdev_files1=np.append(stdev_files1,stdev)
+		beta_vec1 = np.append(beta_vec1, beta)
+
+	N2=1000
+	p2=100
+	t2 = np.arange(2.0, 0.4, -0.2)
+	print(t1)
+
+	files2=np.array([])
+	mean_files2=np.array([])
+	stdev_files2=np.array([])
+	beta_vec2=[]
+
+	for T in t2:
+		files2=np.append(files1, "./Data_files/temp_alpha_0_005{}".format(T))
+		m = np.loadtxt("./Data_files/temp_alpha_0_005{}".format(T), unpack=True)
+		
+		mean = np.mean(m)
+		stdev= np.std(m)
+
+		beta = T
+		mean_files2=np.append(mean_files2,mean)
+		stdev_files2=np.append(stdev_files2,stdev)
+		beta_vec2 = np.append(beta_vec2, beta)
+
+
+
+	plt.plot(beta_vec1, mean_files1, color='black', alpha=0.3)
+	plt.scatter(beta_vec1, mean_files1, color='black', marker='s', alpha=0.3, label="N=1000, $\\alpha$=0.1")
+	plt.fill_between(beta_vec1, mean_files1-stdev_files1, mean_files1+stdev_files1, color='black', alpha=0.1)
+
+	# plt.plot(beta_vec2, mean_files2, color='blue', alpha=0.3)
+	# plt.scatter(beta_vec2, mean_files2, color='blue', marker='*', alpha=0.3, label="N=2000, $\\alpha$=0.05")
+	# plt.fill_between(beta_vec2, mean_files2-stdev_files2, mean_files2+stdev_files2, color='black', alpha=0.1)
+
+
+	plt.plot(beta_vec, mean_files, color='red', alpha=0.8)
+	plt.scatter(beta_vec, mean_files, color='red', alpha=0.8, label="N=4000, $\\alpha$=0.01")
+
+	plt.legend(loc=0)
 	plt.ylabel("Overlap $m_\\mu$")
 	plt.xlabel("T")
 	plt.fill_between(beta_vec, mean_files-stdev_files, mean_files+stdev_files, color='red', alpha=0.3)
 
-	plt.savefig("../Graficos/beta.png")
+
+	plt.savefig("../Graficos/beta2.png")
 	
 def main():
-	ejer_1()
-	#ejer_2()
+	#ejer_1(500)
+	#ejer_1_2()
+	ejer_2()
 	plt.show()
 
 
--- pr-rn-6/Codigo/pr-rn-6_v6.py
+++ pr-rn-6/Codigo/pr-rn-6_v6.py
@@ -26,6 +26,20 @@ def conexion_matrix(patrones, p, N):
 		print("terminado {} {} N={}, p={}".format(i,j, N, p))
 	return matrix_j
 
+
+def conexion_matrix_alpha(patrones, p, N):
+	matrix_j=np.zeros((N,N))
+	for i in range(N) : 
+		for j in range(i+1,N):
+			aux_sum=0
+			for mu in range(p): aux_sum += patrones[mu][i]*patrones[mu][j]/N
+			matrix_j[i][j]= np.sign(aux_sum)
+			matrix_j[j][i]= np.sign(aux_sum)
+
+		print("terminado {} {} N={}, p={}".format(i,j, N, p))
+	return matrix_j
+
+
 ########################################################################################################
 
 def iterar_determ_dynamic(matrix_j, patron_i_mu, N):
@@ -65,6 +79,24 @@ def evol_hopfield_noiseless(N, alpha, datafile):
 
 	print("Terminando N={}, alpha={}".format(N, alpha))
 
+
+def evol_hopfield_noiseless_pesos(N, alpha, datafile):
+	p = int(alpha*N)
+	m= np.zeros(p)
+	
+	patrones = init_matriz(p,N)
+	matrix_j = conexion_matrix_alpha(patrones, p, N)
+
+	outfile = open(datafile, 'w')
+
+	for mu in range(p):	
+		punto_fijo_i_mu = iterar_determ_dynamic(matrix_j, patrones[mu], N)
+		m[mu] = overlap_s_xi(patrones[mu], punto_fijo_i_mu, N)
+		print("overlap {}: {}\n".format(mu, m[mu]))
+		outfile.write("{}\n".format(m[mu]))
+
+	print("Terminando N={}, alpha={}".format(N, alpha))
+
 ########################################################################################################
 
 def ejer_1():
@@ -85,6 +117,28 @@ def ejer_1():
 		plt.hist(m, weights=weights, alpha=0.8, label=filename)
 		plt.legend(loc=0, ncol=2)
 
+##########################33
+
+def ejer_1_2():
+	N_vec=[1000]#[500, 1000 , 2000, 4000]
+	alpha_vec = [0.09, 0.1, 0.12,0.15]#[0.12, 0.14, 0.16, 0.18]
+
+	files = []
+
+	for N in N_vec: 
+		for alpha in alpha_vec: 
+			evol_hopfield_noiseless_pesos( N, alpha, "./Data_files/test_alpha{}_{}".format(N, alpha))
+			files.append("./Data_files/test_alpha{}_{}".format(N, alpha))
+
+	plt.figure(2)
+	for filename  in files:
+		m = np.loadtxt(filename, unpack=True)
+		weights = np.ones_like(m) / len(m)
+		plt.hist(m, weights=weights, alpha=0.8, label=filename)
+		plt.legend(loc=0, ncol=2)
+
+
+
 ################################################################################################
 ################################################################################################
 
@@ -98,22 +152,20 @@ def pr(beta, h_i, signo):
 def iterar_determ_noise(matrix_j, patron_i_mu, N, beta):
 	punto_fijo_i_mu= np.copy(patron_i_mu)
 
-	for unit in range(N):
-		for _ in xrange(1,10):
+	for _ in xrange(1,10):
+		for unit in range(N):
 			h_i_mu=0
 			for mu in range(N): h_i_mu += matrix_j[unit][mu]*punto_fijo_i_mu[mu]
 			prob_plus = pr(beta, h_i_mu, 1.0)
-			prob_mins = pr(beta, h_i_mu, -1.0)
+			#prob_mins = pr(beta, h_i_mu, -1.0)
 
 			prob_norm = random.random() 
 
-			if (prob_plus>=  prob_norm):
+			if (prob_plus >= prob_norm):
 				punto_fijo_i_mu[unit] = 1 
-			elif (prob_mins <= prob_norm) :
+			else:
 				punto_fijo_i_mu[unit] = -1 
 
-	print(punto_fijo_i_mu-patron_i_mu)	
-	print(np.sum(punto_fijo_i_mu-patron_i_mu))
 	overlap= np.dot(punto_fijo_i_mu,patron_i_mu)/N
 	print(overlap)
 	
@@ -123,7 +175,7 @@ def iterar_determ_noise(matrix_j, patron_i_mu, N, beta):
 
 def evol_hopfield_noise(N, p, T, patrones, matrix_j):
 	m=np.zeros(p)
-	outfile= open("./Data_files/temp{}".format(T),'w')
+	outfile= open("./Data_files/temp_alpha_0_005{}".format(T),'w')
 	
 	for mu in range(p):
 		m[mu] = iterar_determ_noise(matrix_j, patrones[mu], N, 1.0/T)
@@ -131,9 +183,11 @@ def evol_hopfield_noise(N, p, T, patrones, matrix_j):
 		outfile.write("{}\n".format(m[mu]))
 
 def ejer_2():
-	N=4000
-	p=40
-	t = [0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.8, 1.9]
+	N=2000
+	p=10
+	#t = np.arange(2.0, 0.1, -0.2)
+	t = np.arange(2.0, 0.1, -0.2)
+	print(t)
 
 	patrones = init_matriz(p,N)
 	matrix_j = conexion_matrix(patrones,p,N)
@@ -142,14 +196,12 @@ def ejer_2():
 		print("Beginning with {}\n".format(T))
 		evol_hopfield_noise(N,p,T, patrones, matrix_j)
 		print("Done with {}\n".format(T))
-		pass
-	#print(t)
 
-	pass
 
 def main():
-	ejer_1()
+	#ejer_1()
 	ejer_2()
+	#ejer_1_2()
 	plt.show()
 
 
